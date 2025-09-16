e5bbde44e327a2d9cb42100e876c839d8038d650
Huthifa  Ahmed Salem Al-fyyad <107212308+Huthifa962@users.noreply.github.com>
2025-09-16 23:03:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        Item coke = new Item("drink", "coke", 1.50);
+        coke.printItem();
+        coke.setSize("LARGE");
+        coke.printItem();
+
+        Item avocado = new Item("Topping","avocado",1.60);
+        avocado.printItem();
+
+        Burger burger = new Burger("regular", 4.00);
+        burger.addToppings("BACON", "CHEESE", "MAYO");
+        Burger burger1 = burger;
+        burger1.printItem();
+
+        MealOrder regularMeal = new MealOrder();
+        regularMeal.addBurgerToppings("BACON","CHEESE","MAYO");
+        regularMeal.setDrinkSize("LARGE");
+        regularMeal.printItemizedList();
+
+        MealOrder secondMeal = new MealOrder("turkey", "7-UP", "chili");
+        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+        secondMeal.setDrinkSize("SMALL");
+        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-UP", "chili");
+        deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE", "CHEESE","MAYO" );
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
+
+    }
 }
+
+
